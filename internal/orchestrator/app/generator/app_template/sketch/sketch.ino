45a631e2a17231bd3d75271b90ca171ca477cc8d
lucarin91 <lucarin@protonmail.com>
2025-10-30 16:05:53 +0100

    feat: improve sketch template

--- internal/orchestrator/app/generator/app_template/sketch/sketch.ino
+++ internal/orchestrator/app/generator/app_template/sketch/sketch.ino
@@ -1,3 +1,15 @@
-void setup() {}
+#include <Arduino_RouterBridge.h>
 
-void loop() {}
+void setup() {
+  // setup the bridge for comunicating with the Linux subsystem.
+  Bridge.begin();
+
+  // start the monitor for debugging logs.
+  Monitor.begin();
+
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}

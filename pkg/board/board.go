7c500df568878d940aeda3a3432568ecae740644
lucarin91 <lucarin@protonmail.com>
2025-12-10 12:22:12 +0100

    chore: remove unused product_name

--- pkg/board/board.go
+++ pkg/board/board.go
@@ -82,7 +82,7 @@ func identifyUnoQ(p *rpc.DetectedPort) {
 }
 
 var onBoard = sync.OnceValue(func() bool {
-	var boardNames = []string{"UNO Q\n", "Imola\n", "Inc. Robotics RB1\n"}
+	var boardNames = []string{"Imola\n", "Inc. Robotics RB1\n"}
 	buf, err := os.ReadFile("/sys/class/dmi/id/product_name")
 	if err == nil && slices.Contains(boardNames, string(buf)) {
 		return true

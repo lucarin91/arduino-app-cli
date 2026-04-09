7d404492b3dd3c1f79c22e7dd69df6175949081f
lucarin91 <lucarin@protonmail.com>
2026-04-09 11:56:31 +0200

    add test

---
+++ internal/platform/platform_test.go
@@ -0,0 +1,26 @@
+package platform
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/arduino/go-paths-helper"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestGetPlatformWithOverride(t *testing.T) {
+	tmpDir := paths.New(t.TempDir())
+	override := Platform{
+		FQBN: "some:custom:board",
+	}
+
+	f, err := tmpDir.Join("platform.json").Create()
+	require.NoError(t, err)
+	defer f.Close()
+	err = json.NewEncoder(f).Encode(override)
+	require.NoError(t, err)
+
+	p := GetPlatform(tmpDir)
+	assert.Equal(t, "some:custom:board", p.FQBN)
+}

eb72a92572d749723462605b477fa521ce9edc93
lucarin91 <l.rinaldi@arduino.cc>
2026-04-29 10:32:11 +0200

    allow events to be called before apply

--- internal/update/update.go
+++ internal/update/update.go
@@ -173,16 +173,6 @@ func (m *Manager) UpgradePackages(ctx context.Context, pkgs []UpgradablePackage)
 // Subscribe creates a new channel for receiving APT events.
 func (b *Manager) Subscribe() chan Event {
 	eventCh := make(chan Event, 100)
-
-	// return a closed channel if there isn't an update in progress, to avoid subscribers to wait for events that will never come.
-	b.isUgradingMutex.RLock()
-	if !b.isUpgrading {
-		defer b.isUgradingMutex.RUnlock()
-		close(eventCh)
-		return eventCh
-	}
-	b.isUgradingMutex.RUnlock()
-
 	b.mu.Lock()
 	defer b.mu.Unlock()
 	b.subs[eventCh] = struct{}{}

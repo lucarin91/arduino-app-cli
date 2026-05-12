8be69fcf3228bf6498def9d6f600e7b0a65ade3e
lucarin91 <l.rinaldi@arduino.cc>
2026-05-12 10:21:17 +0200

    add license header

--- pkg/board/remote/fs.go
+++ pkg/board/remote/fs.go
@@ -1,3 +1,8 @@
+// This file is part of arduino-app-cli.
+//
+// SPDX-FileCopyrightText: Arduino s.r.l. and/or its affiliated companies
+// SPDX-License-Identifier: GPL-3.0-or-later
+
 package remote
 
 import (
